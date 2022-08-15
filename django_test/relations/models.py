ec88ed4b10f8c7497568fe32d308f7317ee8fa0c
aye sanchez <ayelen.sanchez@gmail.com>
2022-08-15 17:12:01 -0300

    class Car_driver

--- django_test/relations/models.py
+++ django_test/relations/models.py
@@ -10,10 +10,13 @@ class Car_owner(models.Model):
 
 class Colors(models.Model):
     name = models.CharField(max_length=50)
-
-
 class Car(models.Model):
     owner = models.OneToOneField(Car_owner, on_delete=models.CASCADE, related_name='owned_car') #suponemos que tiene un solo dueño #le decimos a que modelo estar relacionado (en este caso Car_owner), y que pasa cuando borramos uno (con on_delete). En este caso si yo borro un dueño tambien borrame los datos del auto, o si yo borro un auto borrame los datos del dueño.
     color = models.ForeignKey(Colors, on_delete=models.SET_NULL, null=True, blank=True, related_name='car') #le decimos que esta relacionado con colores con "Colors"; le decimos que pase si borro un color con "on_delete" con SET_NULL le indico que el campo es nulo / la otra opcion es CASCADE que significa borra el auto / y la otra opcion es RESTRIC que significa no dejes borrar un color si hay autos asignados.
     #ademas le tengo que aclarar que el campo puede ser nulo cuando borro un color con: null=True y blank=True
     #que pasa si a un color, le quiero pedir todos los autos que tengan ese color, tengo que unsar related_name='car'; este 'car' lo usamos para poder usarlo desde class Colors, si nosotros decimos Colors.car me va a traer dos autos de los cuales sea un color
+
+class Car_driver(models.Model): #podemos tener un conductor que conduzca muchos autos o un auto que sea conducido por muchas personas
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    cars = models.ManyToManyField(Car, related_name='drivers')
